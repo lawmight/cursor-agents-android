dbea88b5102ebf1b1e341fc9165d78a63ba887b7
Tom Coustols <tom.coustols@tcdynamics.fr>
2026-05-11 19:27:27 +0700

    scaffold: app/src/main/kotlin/fr/lawmight/cursoragents/di/AppModule.kt

---
+++ app/src/main/kotlin/fr/lawmight/cursoragents/di/AppModule.kt
@@ -0,0 +1,20 @@
+package fr.lawmight.cursoragents.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import fr.lawmight.cursoragents.data.api.CursorApiClient
+import fr.lawmight.cursoragents.data.auth.EncryptedKeyStore
+import fr.lawmight.cursoragents.data.repository.AgentsRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideAgentsRepository(keyStore: EncryptedKeyStore): AgentsRepository =
+        AgentsRepository { alias -> keyStore.read(alias)?.let(::CursorApiClient) }
+}
