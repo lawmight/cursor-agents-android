375392b936120884413d631470ac0b6968fcde0c
Tom Coustols <tom.coustols@tcdynamics.fr>
2026-05-11 19:27:32 +0700

    scaffold: app/src/test/kotlin/fr/lawmight/cursoragents/SmokeTest.kt

---
+++ app/src/test/kotlin/fr/lawmight/cursoragents/SmokeTest.kt
@@ -0,0 +1,8 @@
+package fr.lawmight.cursoragents
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class SmokeTest {
+    @Test fun `placeholder so unit-test task runs`() = assertTrue(true)
+}
